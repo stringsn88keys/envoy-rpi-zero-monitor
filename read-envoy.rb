8f80eb74a6762a2f39ade74b3ea2a76505207a5b
Thomas Powell <tpowell@appriss.com>
2019-08-28 18:32:39 -0500

    script to check envoy and call home

---
+++ read-envoy.rb
@@ -0,0 +1,23 @@
+require 'gmail'
+require 'net/http'
+require 'json'
+
+eval File.read('.config')
+
+data = Net::HTTP.get('envoy.local', '/production.json')
+
+parsed_data = JSON.parse(data)
+production_data=parsed_data['production'][0]
+active_count=production_data['activeCount']
+watts_now=production_data['wNow']
+
+exit unless active_count==0 || watts_now==0
+gmail = Gmail.new(USERNAME, PASSWORD) do |gmail|
+  gmail.deliver do
+    to TO_EMAIL
+    subject "#{production_data['activeCount']} inverters, #{production_data['wNow']} watts"
+    text_part do
+      body data
+    end
+  end
+end
