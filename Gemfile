488a2d768d91ece44e4d4d3fc0f25adf14fe99a7
Thomas Powell <tpowell@appriss.com>
2019-11-26 16:01:49 -0600

    Added Nexmo and sunrise/sunset hooks

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'ruby-gmail'
+gem 'tzinfo'
+gem 'RubySunrise'
+gem 'nexmo'
--- read-envoy.rb
+++ read-envoy.rb
@@ -1,18 +1,44 @@
 require 'gmail'
 require 'net/http'
 require 'json'
+require 'solareventcalculator'
+require 'date'
+require 'nexmo'
 
 eval File.read('.config')
 
-data = Net::HTTP.get('envoy.local', '/production.json')
+data = Net::HTTP.get(ENVOY_HOST, '/production.json')
 
 parsed_data = JSON.parse(data)
 production_data=parsed_data['production'][0]
 active_count=production_data['activeCount']
 watts_now=production_data['wNow']
+wh_lifetime=production_data['whLifetime']
 
-exit unless active_count<INVERTER_COUNT || watts_now==0
-gmail = Gmail.new(USERNAME, PASSWORD) do |gmail|
+sms_message="Solar output down!"
+
+client = Nexmo::Client.new(
+  api_key: NEXMO_API_KEY,
+  api_secret: NEXMO_API_SECRET
+)
+
+sec = SolarEventCalculator.new(Date.today, LATITUDE, LONGITUDE)
+tz=TZInfo::Timezone.get(TZ)
+sunrise = tz.to_local(sec.compute_utc_official_sunrise)
+sunset = tz.to_local(sec.compute_utc_official_sunset)
+time_now = tz.to_local(DateTime.now)
+
+exit unless active_count < INVERTER_COUNT || watts_now==0
+
+if time_now > sunrise && time_now < sunset
+  client.sms.send(
+    from: NEXMO_SMS_FROM,
+    to: NEXMO_SMS_TO,
+    text: sms_message
+  )
+end
+
+Gmail.new(USERNAME, PASSWORD) do |gmail|
   gmail.deliver do
     to TO_EMAIL
     subject "#{production_data['activeCount']} inverters, #{production_data['wNow']} watts"
